35faba07d5a1f178e033f57433d536dcdc2e11ca
Benji Visser <benji@093b.org>
2023-09-14 17:20:02 -0400

    Update plans.js

--- src/plans.js
+++ src/plans.js
@@ -7,7 +7,7 @@ function freeProSubscription(login) {
 }
 
 function invoicedProSubscription(login) {
-  const organizations = ['pace-int', 'honestbank', 'MacPaw'];
+  const organizations = ['pace-int', 'honestbank', 'MacPaw', 'stoplightio'];
   const match = organizations.find((o) => o.toLowerCase() === String(login).toLowerCase());
   return match !== undefined;
 }

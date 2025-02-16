98d3f8b5bcdfb8e015062b9223c283a2ba21b9e5
Benji Visser <benji@093b.org>
2025-02-15 20:48:17 -0500

    lint
    Signed-off-by: Benji Visser <benji@093b.org>

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,23 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {ignores: [".old/**/*", "dist/**/*", "lib/**/*", "node_modules/**/*", "eslint.config.js", "jest.config.js"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+  },
+  {
+    ignores: [
+      ".output/**/*",
+      ".nitro/**/*",
+      "dist/**/*",
+      "lib/**/*",
+      "node_modules/**/*",
+      "eslint.config.js",
+      "jest.config.js",
+    ],
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
